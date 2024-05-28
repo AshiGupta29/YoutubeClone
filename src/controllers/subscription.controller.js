826bc102c060ebc769c477484d0fa031635d7ea0
AshiGupta29 <ashigupta0529@gmail.com>
2024-05-28 17:22:28 +0530

    add subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,19 +7,124 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
-})
-
-// controller to return subscriber list of a channel
-const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-})
-
-// controller to return channel list to which user has subscribed
-const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-})
+    const { channelId } = req.params;
+    if (!channelId) {
+      throw new ApiError(400, "Channel Id is required");
+    }
+    if (!isValidObjectId(channelId)) {
+      throw new ApiError(400, "Channel Id is not valid");
+    }
+    const userId  = req.user?._id;
+    const credential = { subscriber: userId, channel: channelId };
+  
+    try {
+      const subscribed = await Subscription.findOne(credential);
+      if (!subscribed) {
+        const newSubcription = await Subscription.create(credential);
+        if (!newSubcription) {
+          throw new ApiError(500, "Unable to subscribe to channel");
+        }
+        return res
+          .status(201)
+          .json(new ApiResponse(201, "Subscription successful", newSubcription));
+      }else{
+          const deletedSubscription = await Subscription.deleteOne(credential);
+           if(!deletedSubscription){
+               throw new ApiError(500,"Unable to Unsubscribe channel")
+           }
+           return res
+           .status(200)
+           .json(new ApiResponse(200,deletedSubscription,"Channel Unsubscribed Successfully!!"))
+      }
+    } catch (error) {
+      throw new ApiError(500, error.message || "Unable to subscription");
+    }
+  });
+  
+  const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {subscriberId} = req.params
+      if(!subscriberId){
+          throw new ApiError(400,"subscriberId is Requitred!!")
+      }
+      if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Subscriber Id is not valid");
+      }
+     try {
+     
+       const subscribers = await Subscription.aggregate([{
+           $match:{
+               channel : new mongoose.Types.ObjectId(subscriberId)
+           },
+       },{
+           $group:{
+               _id:"channel",
+               subscribers:{$push:"$subscriber"}
+           }
+       },{
+           $project:{
+               _id:0,
+               subscribers:1
+           }
+       }])
+       
+       if(!subscribers || subscribers.length === 0 ){
+           return res
+           .status(200)
+           .json(new ApiResponse(200, [], "No subscribers found for the channel"));
+   
+       }
+       return res
+       .status(200)
+       .json(new ApiResponse(200,subscribers,"All Subscribers fetched Successfully!!"))
+       
+     } catch (e) {
+      throw new ApiError(500,e?.message || "Unable te fetch subscribers!")
+      
+     }
+  });
+  
+  const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { channelId } = req.params
+      if(!channelId){
+          throw new ApiError(400,"Channel Id is Requitred!!")
+      }
+      if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Channel Id is not valid");
+      }
+      try {
+     
+          const subscribedChannels = await Subscription.aggregate([{
+              $match:{
+                  subscriber : new mongoose.Types.ObjectId(channelId)
+              },
+          },{
+              $group:{
+                  _id:"subscriber",
+                  subscribedChannels:{$push:"$channel"}
+              }
+          },{
+              $project:{
+                  _id:0,
+                  subscribedChannels:1
+              }
+          }])
+          
+          if(!subscribedChannels || subscribedChannels.length === 0 ){
+              return res
+              .status(200)
+              .json(new ApiResponse(200, [], "No subscribedChannel found for the user"));
+      
+          }
+          return res
+          .status(200)
+          .json(new ApiResponse(200,subscribedChannels,"All SubscribedChannels fetched Successfully!!"))
+          
+        } catch (e) {
+         throw new ApiError(500,e?.message || "Unable te fetch subscribedChannels!")
+         
+        }
+  });
+  
 
 export {
     toggleSubscription,
